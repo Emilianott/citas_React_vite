6b158c06692d76d6ae25abd567a0dccefc485e9c
Roberto Tejeda <emilianott19@gmail.com>
2023-01-06 19:23:47 -0400

    deployment de la web

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export const Header = () => {
+  return (
+    
+      <h1 className="font-black text-5xl text-center  max-auto ">Seguimientos Pacientes{" "} 
+        <span className="text-indigo-600">Veterinaria</span></h1>
+    
+  )
+}
